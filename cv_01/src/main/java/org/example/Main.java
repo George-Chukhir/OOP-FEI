d6305b450a49176f98daa9a4e38125c51fade884
Heorhii Chukhir <g.chuchir@gmail.com>
2025-02-21 19:26:29 +0100

    initial commit

--- cv_01/src/main/java/org/example/Main.java
+++ cv_01/src/main/java/org/example/Main.java
@@ -1,11 +1,27 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            System.out.println("i = " + i);
+import org.w3c.dom.ls.LSOutput;
+
+public class Main { //class
+    public static void main(String[] args) { //method
+
+        int[] arr = new int[10]; //Static Array
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+            System.out.printf("arr[%d] = %d\n", i, 2*arr[i]);
         }
 
+        for (int i = 0; i < arr.length; i++) {
+           // System.out.printf("arr[%d] = %d\n", i, 2*arr[i]);
+                cout(arr[i]);
+        }
+
+
+
+    }
+
+    public static void cout(int a){ //method
+        System.out.println("arr[i] = " + a);
     }
 
 }
