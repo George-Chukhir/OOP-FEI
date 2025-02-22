478e38edb05107cee06e94f7d2e113fed70bbc20
Heorhii Chukhir <g.chuchir@gmail.com>
2025-02-22 14:47:43 +0100

    initial commit

--- cv_01/src/main/java/org/example/Main.java
+++ cv_01/src/main/java/org/example/Main.java
@@ -1,27 +1,36 @@
 package org.example;
-
 import org.w3c.dom.ls.LSOutput;
+import java.io.PrintWriter;
 
 public class Main { //class
     public static void main(String[] args) { //method
 
         int[] arr = new int[10]; //Static Array
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i;
-            System.out.printf("arr[%d] = %d\n", i, 2*arr[i]);
-        }
+        fillAndPrintArray(arr);
 
-        for (int i = 0; i < arr.length; i++) {
-           // System.out.printf("arr[%d] = %d\n", i, 2*arr[i]);
-                cout(arr[i]);
-        }
-
-
-
+        String[] cars = {"Volvo", "BMW", "Ford"};
+        arrayOfCars(cars);
     }
 
-    public static void cout(int a){ //method
-        System.out.println("arr[i] = " + a);
+
+    public static void fillAndPrintArray(int[] array){ //method
+        //filling
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+
+        //printing
+        for (int i = 0; i < array.length; i++) {
+            System.out.printf("Arr[%d] = %d\n", i, array[i]);
+        }
+    }
+
+    public static void arrayOfCars(String[] array){ //method
+        int index = 1;
+        for (String i : array) {
+            System.out.printf("Car %d is: %s\n", index,  i);
+            index++;
+        }
     }
 
 }
